91ad2ca50e00c5050c446ca0356919b634b9781a
ivywang122 <mucha1202@gmail.com>
2018-05-07 16:38:38 +0800

    Upload EmailVerification.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace EmailVerification
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.EmailTectBox = new System.Windows.Forms.TextBox();
+            this.VerifyButton = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("微軟正黑體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TitleLabel.Location = new System.Drawing.Point(206, 36);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(390, 81);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "請輸入Email";
+            // 
+            // EmailTectBox
+            // 
+            this.EmailTectBox.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.EmailTectBox.Location = new System.Drawing.Point(47, 153);
+            this.EmailTectBox.Name = "EmailTectBox";
+            this.EmailTectBox.Size = new System.Drawing.Size(693, 71);
+            this.EmailTectBox.TabIndex = 1;
+            this.EmailTectBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.EmailTectBox_KeyDown);
+            // 
+            // VerifyButton
+            // 
+            this.VerifyButton.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.VerifyButton.Location = new System.Drawing.Point(215, 264);
+            this.VerifyButton.Name = "VerifyButton";
+            this.VerifyButton.Size = new System.Drawing.Size(361, 98);
+            this.VerifyButton.TabIndex = 2;
+            this.VerifyButton.Text = "驗   證";
+            this.VerifyButton.UseVisualStyleBackColor = true;
+            this.VerifyButton.Click += new System.EventHandler(this.VerifyButton_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(43, 435);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 24);
+            this.ResultLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.VerifyButton);
+            this.Controls.Add(this.EmailTectBox);
+            this.Controls.Add(this.TitleLabel);
+            this.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.TextBox EmailTectBox;
+        private System.Windows.Forms.Button VerifyButton;
+        private System.Windows.Forms.Label ResultLabel;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmailVerification
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EmailTectBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) VerifyButton_Click(this, null);
+        }
+
+        private void VerifyButton_Click(object sender, EventArgs e)
+        {
+            String email = EmailTectBox.Text;
+            Color red = System.Drawing.Color.Red;
+            Color green = System.Drawing.Color.Green;
+
+            Regex rgx = new Regex(@"^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$");
+
+            if (String.IsNullOrEmpty(email))
+            {
+                ResultLabel.Text = "請確實輸入Email! ";
+                ResultLabel.ForeColor = red;
+            }
+            else
+            {
+                /*
+                int atIndex = email.IndexOf("@");
+                int dotIndex = email.IndexOf(".");
+                if (atIndex != -1 && dotIndex != -1)
+                {
+                    if (atIndex > 0 && dotIndex < email.Length - 2 && atIndex < dotIndex)
+                    {
+                        ResultLabel.Text = "驗證成功!";
+                        ResultLabel.ForeColor = green;
+                    }
+                    else {
+                        ResultLabel.Text = "格式不正確，請重新輸入";
+                        ResultLabel.ForeColor = red;
+                    }
+                }
+
+                */
+                if (IsEmail(email, rgx))
+                {
+                    ResultLabel.Text = "驗證成功!";
+                    ResultLabel.ForeColor = green;
+                }
+                else
+                {
+                    ResultLabel.Text = "格式不正確，請重新輸入";
+                    ResultLabel.ForeColor = red;
+                }
+            }
+        }
+
+        public Boolean IsEmail(string s, Regex rgx)
+        {
+            return rgx.IsMatch(s);
+        }
+    }
+}
